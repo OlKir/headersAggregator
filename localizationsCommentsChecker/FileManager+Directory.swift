29dcfc534ad89c79f9436e9bf3a65ba75bbf17a4
Oleksii Kirizii <oleksii@bikemap.net>
2021-11-05 16:06:28 +0100

    Implement command line utility to check interface localizations comments. IOS-4246

---
+++ localizationsCommentsChecker/FileManager+Directory.swift
@@ -0,0 +1,19 @@
+//
+//  FileManager+Directory.swift
+//  localizationsCommentsChecker
+//
+//  Created by Oleksii on 05.11.21.
+//
+
+import Foundation
+
+extension FileManager {
+    func isDirectory(atPath: String) -> Bool {
+        var check: ObjCBool = false
+        if fileExists(atPath: atPath, isDirectory: &check) {
+            return check.boolValue
+        } else {
+            return false
+        }
+    }
+}
--- localizationsCommentsChecker/main.swift
+++ localizationsCommentsChecker/main.swift
@@ -7,5 +7,39 @@
 
 import Foundation
 
-print("Hello, World!")
+var inputPath: String?
+
+for i in 0..<CommandLine.arguments.count {
+  if i == 1 {
+    inputPath = CommandLine.arguments[i]
+  }
+}
+
+guard
+  CommandLine.arguments.count > 0,
+  let scanPath = inputPath
+else {
+  print("   ERROR: provide folder path for recursive scan")
+  exit(1)
+}
+
+let fileManager = FileManager()
+var isDirectory: ObjCBool = false
+if fileManager.fileExists(atPath: scanPath, isDirectory: &isDirectory) {
+  guard isDirectory.boolValue else {
+    print("   ERROR: please provide path to folder")
+    exit(1)
+  }
+  let startTime = Date()
+  print("Scan localization comments in interface files started...")
+
+  LocalizationsScanner.scanFolder(scanPath)
+
+  print("Scan localization comments finished in \(Date().timeIntervalSince(startTime)) seconds.")
+} else {
+  print("   ERROR: please provide a valid path to folder")
+  exit(1)
+}
+
+
 
